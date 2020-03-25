2799b4660db8beee07d5d45a459229154a1a8645
AnnaBucher <62422693+AnnaBucher@users.noreply.github.com>
2020-03-25 16:42:54 +0100

    Update NFS1_IP.R
    200325_erster Teil DEP

--- NFS1_IP.R
+++ NFS1_IP.R
@@ -1,3 +1,17 @@
 Interactome <- read.csv("Fly_Interactome_curated.csv", header=TRUE, sep=",")
 expdesign <- read.table("NFS1_expdesign.txt", header = TRUE, sep="\t")
-proteinGroups <- read.table("proteinGroups.txt", header = TRUE, sep="\t") #irgendwas filtern?
+proteinGroups <- read.table("proteinGroups.txt", header = TRUE, sep="\t") 
+
+data <- filter(proteinGroups, Reverse != "+", Potential.contaminant != "+")
+dim(data)
+colnames(data)
+
+data$Gene.names %>% duplicated() %>% any()
+data %>% group_by(Gene.names) %>% summarize(frequency = n()) %>% 
+  arrange(desc(frequency)) %>% filter(frequency > 1)
+data_unique <- make_unique(data, "Gene.names", "Protein.IDs", delim = ";")
+data$name %>% duplicated() %>% any()
+
+LFQ_columns <- grep("LFQ.", colnames(data_unique)) # get LFQ column numbers
+experimental_design <- UbiLength_ExpDesign
+data_se <- make_se(data_unique, LFQ_columns, experimental_design)
