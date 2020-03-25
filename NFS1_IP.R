7d8d3daa4487900bddcf1019eb95d1d262c1e363
AnnaBucher <62422693+AnnaBucher@users.noreply.github.com>
2020-03-25 15:13:29 +0100

    Update NFS1_IP.R
    200325_alle Dateien einlesen

---
+++ NFS1_IP.R
@@ -0,0 +1,3 @@
+Interactome <- read.csv("Fly_Interactome_curated.csv", header=TRUE, sep=",")
+expdesign <- read.table("NFS1_expdesign.txt", header = TRUE, sep="\t")
+proteinGroups <- read.table("proteinGroups.txt", header = TRUE, sep="\t") #irgendwas filtern?
